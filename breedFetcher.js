db8eec17079c3444abee391604e0fe9003a1a036
Pauline Shen <s9633291@gmail.com>
2022-09-09 04:00:53 +0000

    refactored code and added test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,19 @@
 const request = require('request');
-const args = process.argv;
 
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + args[2], (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  if (body === '[]') {
-    console.log('The requested breed is not found.');
-    return;
-  }
-  const data = JSON.parse(body);
-  console.log(data[0]['description']);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    if (body === '[]') {
+      return callback('The requested breed is not found.', null);
+    }
+    const data = JSON.parse(body);
+    return callback(null, data[0]['description']);
+  });
+};
+
+
+
+module.exports = { fetchBreedDescription };
